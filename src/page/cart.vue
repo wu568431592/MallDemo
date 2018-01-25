18601c7222ac708b96c6f1fe96879fd81e542bde
wu568431592 <wuwenchao0314@163.com>
2018-01-25 18:02:30 +0800

    new push

--- src/page/cart.vue
+++ src/page/cart.vue
@@ -5,7 +5,7 @@ s<template>
         <ul>
           <li v-for="i in 5">
             <div class="store">
-              <input type="checkbox">
+              <input type="checkbox" @click="checkStoreAll($event)">
               <i class="icon iconfont icon-store"></i>
               <span>我是店铺名称</span>
             </div>
@@ -17,11 +17,11 @@ s<template>
                 </div>
                 <div class="proInfo">
                   <h5>尤卓尔 丁酸氢化可的松乳膏10g/盒  商品名最多两行...</h5>
-                  <p>规格：1盒装10g</p>
+                  <p :class="{active:isedit}" @click="changeType($event)">规格：1盒装10g</p>
                   <span>&yen;15</span>
-                  <button class="add">+</button>
+                  <button class="add" @click="addFun($event)">+</button>
                   <input type="number" value="1">
-                  <button class="reduce">-</button>
+                  <button class="reduce" @click="reduceFun($event)">-</button>
                 </div>
               </li>
             </ul>
@@ -30,7 +30,7 @@ s<template>
       </div>
       <div class="countBox">
         <div class="checkboxBox">
-          <input type="checkbox" id="checkAll" name="checkAll">
+          <input type="checkbox" id="checkAll" name="checkAll" @click="checkAll($event)">
           <label for="checkAll">全选</label>
         </div>
         <div class="countNow" v-show="!isedit">
@@ -43,18 +43,24 @@ s<template>
           <button>删除</button>
         </div>
       </div>
+      <chooseItemAlert v-show="itemEdit" @hideChooseAlert="hideChooseAlert"></chooseItemAlert>
+      <div v-show="isCoverShow" class="cover">
+      </div>
     </div>
 </template>
 
 <script>
     import headerBox from  '../components/headerBox/headerBox.vue'
+    import chooseItemAlert from  '../components/chooseItemAlert/chooseItemAlert.vue'
     export default {
       name:'cart',
-      components:{headerBox},
+      components:{headerBox,chooseItemAlert},
       data(){
         return{
           rightButtonList:['messageBtn'],
           isedit:false,
+          itemEdit:false,
+          isCoverShow:false
         }
       },
       methods:{
@@ -63,11 +69,65 @@ s<template>
         },
         closeeidt:function(){
           this.isedit = true;
+        },
+        addFun:function(e){
+          var me = e.target;
+          me.nextElementSibling.value ++;
+        },
+        reduceFun:function(e){
+          var me = e.target;
+          if(me.previousElementSibling.value>1){
+            me.previousElementSibling.value --
+          }
+        },
+        hideChooseAlert:function(){
+          var me = document.getElementsByClassName('chooseItemAlert')[0];
+          me.setAttribute('class','chooseItemAlert animated slideOutDown');
+          this.isCoverShow =false;
+          setTimeout(function(){
+            this.itemEdit = false;
+          },2000)
+        },
+        changeType:function(e){
+          if(e.target.getAttribute('class') == 'active'){
+            var me = document.getElementsByClassName('chooseItemAlert')[0]
+            me.setAttribute('class','chooseItemAlert animated slideInUp')
+            this.itemEdit = true;
+            this.isCoverShow = true;
+          }
+        },
+        checkStoreAll:function(e){
+            var me =e.target;
+            var lis = me.parentNode.nextElementSibling.children;
+            var ischecked = me.checked;
+            for(var i = 0; i<lis.length;i++){
+              if(ischecked == true){
+                lis[i].children[0].setAttribute('checked','true')
+              }
+              if(ischecked == false){
+                lis[i].children[0].setAttribute('checked',false)
+                lis[i].children[0].removeAttribute('checked')
+              }
+            }
+        },
+        checkAll:function(e){
+          var me = e.target;
+          var check= document.getElementsByTagName('input');
+          for (var i=0;i<check.length ;i++){
+            if(check[i].type == "checkbox"){
+              if(me.checked == true){
+                check[i].setAttribute('checked',true);
+              }
+              if(me.checked == false){
+                check[i].setAttribute('checked',false);
+                check[i].removeAttribute('checked');
+              }
+            }
+          }
         }
       },
       beforeMount:function(){
         this.$emit('showTabBar');
-
       },
     }
 </script>
@@ -77,6 +137,15 @@ s<template>
   .headerBox{
     width:100%;
     position:fixed;
+    top:0px;
+    left:0px;
+  }
+  .cover{
+    position: fixed;
+    background-color: rgba(0,0,0,0.5);
+    z-index: 1000000;
+    width:100vw;
+    height:100vh;
     top:0px;
     left:0px;
   }
@@ -210,10 +279,17 @@ s<template>
                       margin-bottom: 1vh;
                     }
                     p{
-                      line-height: 3vh;
-                      height:3vh;
+                      line-height: 3.5vh;
+                      height:3.5vh;
                       color:#999;
                       margin-bottom: 1vh;
+                      padding:0px 5px;
+                      border:1px solid transparent;
+                      border-radius:5px;
+                    }
+                    p.active{
+                      border:1px solid #999;
+                      -webkit-border-radius:5px;
                     }
                     span{
                       color:#e4393c;
